9d17f62745bee6d700b72d33a38d2b5e2e6c5d34
user-emanoel-fuentes <worknoel.cardoso@gmail.com>
2025-05-13 20:22:10 -0300

    feat: Add python files.

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'estate',
+    'version': '1.0.17.0',
+    'description': 'Module developed in the Odoo technical training course.',
+    'author': 'Emanoel Fuentes',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+    'application': True,
+}
